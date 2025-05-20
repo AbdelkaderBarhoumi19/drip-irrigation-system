f92dd1e03006f84eb7f721365e15e5fdb979c590
AbdelkaderBarhoumi19 <barhoumiabdelkader45@gmail.com>
2025-05-20 15:03:14 +0100

    Add DHT22 monitoring script

---
+++ dht22_monitor.ino
@@ -0,0 +1,33 @@
+// File: dht22_monitor.ino
+
+#include "DHT.h"
+
+#define DHTPIN 2       // Digital pin connected to the DHT22
+#define DHTTYPE DHT22  // Define sensor type
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+}
+
+void loop() {
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature(); // in °C
+
+  if (isnan(humidity) || isnan(temperature)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  Serial.print("Humidity: ");
+  Serial.print(humidity);
+  Serial.print(" %\t");
+
+  Serial.print("Temperature: ");
+  Serial.print(temperature);
+  Serial.println(" °C");
+
+  delay(2000);
+}
